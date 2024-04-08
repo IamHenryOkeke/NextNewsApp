3299af9111ea96159a555ae5eb1e0cf0bc0528d0
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2024-04-08 18:42:48 +0100

    fetch bug fix

--- app/news/top-headlines/Feed.tsx
+++ app/news/top-headlines/Feed.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { getData } from '@/api/api'
+import { getData, getTopHeadlinesData } from '@/api/api'
 import Articles from '@/components/Articles'
 import { TopHeadlinesSearchParamsProptypes } from '@/types/types'
 
 export default async function Feed({searchParams}: TopHeadlinesSearchParamsProptypes) {
-  const data = await getData(searchParams.country, searchParams.category)
+  const data = await getTopHeadlinesData(searchParams.country, searchParams.category)
   return (
     <Articles data={data} />
   )
--- app/news/top-headlines/page.tsx
+++ app/news/top-headlines/page.tsx
@@ -1,12 +1,10 @@
 import React, { Suspense } from 'react'
 import TopHeadlinSearch from '@/components/TopHeadlinSearch'
 import { TopHeadlinesSearchParamsProptypes } from '@/types/types'
-import { getTopHeadlinesData } from '@/api/api'
 import Loading from '../loading'
 import Feed from './Feed'
 
 export default async function page({ searchParams }: TopHeadlinesSearchParamsProptypes) {
-  const data = await getTopHeadlinesData(searchParams.country, searchParams.category)
   return (
     <div>
       <TopHeadlinSearch />
