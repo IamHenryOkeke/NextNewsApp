ae1e4a03e233d7f86b398811e34c8f1a027f8816
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2024-04-10 14:19:20 +0100

    handling null urlImage error

--- components/NewsDetail.tsx
+++ components/NewsDetail.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import { NewsProp } from '@/types/types'
+import Image from 'next/image'
+import NoImage from '../app/assets/Broken-image.png'
 
+import { Merriweather } from 'next/font/google'
+
+const merriweather = Merriweather({
+  weight: ['300', '400', '700', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 interface NewsItemProp {
   newsItem : NewsProp | undefined
 }
@@ -8,10 +17,10 @@ interface NewsItemProp {
 export default function NewsDetail({ newsItem }: NewsItemProp) {
   return (
     <div className='space-y-4'>
-      <img className='rounded-lg' src={newsItem?.urlToImage} alt="news image" />
-      <h2 className='text-xl md:text-2xl font-bold'>{newsItem?.title}</h2>
+      <h2 className={`${merriweather.className} text-xl md:text-2xl font-bold`}>{newsItem?.title}</h2>
       <span>Published date: {newsItem?.publishedAt.slice(0, 10)}</span>
-      <h3 className='text-lg font-semibold'>Author: {newsItem?.author}</h3>
+      {newsItem?.urlToImage ? <img className='rounded-lg' src={newsItem?.urlToImage} alt="news image" /> : <Image src={NoImage} alt='No image' className='mx-auto md:h-96'/>}
+      <h3 className='text-sm md:text-lg font-bold'>By {newsItem?.author ? newsItem?.author : "No author"}</h3>
       <span>Source: {newsItem?.source.name}</span>
       <p>{newsItem?.description}</p>
       <a className='underline hover:text-emerald-300 hover:no-underline' href={newsItem?.url} target="_blank" rel="noopener noreferrer">Read more</a>
